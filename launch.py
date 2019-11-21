468f0d8849cef1ba33c3485c846ab6040dcaae4f
jianjin xu <jianjinxu@dyn-128-59-157-27.dyn.columbia.edu>
2019-11-20 20:48:57 -0500

    sync

--- launch.py
+++ launch.py
@@ -1,6 +1,6 @@
 import os
 
 gpus = [0, 1, 2, 3, 4, 5, 6, 7]
-basecmd = "CUDA_VISIBLE_DEVICES=%d python encode_images.py ~/data reconstruction dlatent noise --start %d --end %d &"
+basecmd = "CUDA_VISIBLE_DEVICES=%d python encode_images.py ~/data/CelebAMask-HQ/CelebA-HQ-img reconstruction dlatent noise --start %d --end %d &"
 for i in range(len(gpus)):
     os.system(basecmd % (gpus[i], i * 100, (i + 1) * 100))
